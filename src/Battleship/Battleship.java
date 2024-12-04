9de767927bb3bbe76a074945e1fea836a4210390
Josuexili <josue.gonzalez.santacreu@gmail.com>
2024-12-04 15:29:57 +0100

    hola

--- src/Battleship/Battleship.java
+++ src/Battleship/Battleship.java
@@ -2,7 +2,7 @@ package Battleship;
 
 import java.util.Scanner;
 import java.util.Random;
-
+//Hola que tal.
 public class Battleship {
 	public static void main(String[] args) {
 		Scanner in = new Scanner (System.in);
